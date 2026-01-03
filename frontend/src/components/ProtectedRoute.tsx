776238da0889aa3df49d40c043f04ea22febeb56
Thai <minh.thai.tran@mail.mcgill.ca>
2026-01-03 04:39:50 -0500

    protected rounte for unauthenticated user

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { useAuth } from './AuthProvider'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const { user } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/')
+    }
+  }, [user, router])
+
+  if (!user) return null
+
+  return <>{children}</>
+}
